4013f77ad8b5b5643a1f9aa0172fe5e2b15b380b
paul-kibethi <2000kibethipaul@gmail.com>
2024-03-28 15:14:40 +0300

    shoe class

--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,25 @@
 #!/usr/bin/env python3
 
+import sys
+
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+        self.brand = brand
+        self._size = None
+        self.size = size
+        self.repaired = False
+
+    @property
+    def size(self):
+        return self._size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            print("size must be an integer", file=sys.stdout)
+        else:
+            self._size = value
+
+    def cobble(self):
+        self.repaired = True
+        print("Your shoe is as good as new!\n")
